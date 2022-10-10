ca9222bc5b04dea3b5d52e75c8df563402fda877
rialgou <rialgou@gmail.com>
2022-10-09 23:21:20 -0300

    avance

--- t3.py
+++ t3.py
@@ -13,8 +13,11 @@ arcos=[(i,j) for i in nodos for j in nodos if i!=j]
 np.random.seed(0)
 q={n:np.random.randint(10,15) for n in clientes}
 q[0]=0
-Q=50
 
+#Vehiculos
+vehiculos = [1,2,3,4]
+#Q=50
+Q ={1:50,2:50,3:25,4:25}
 #Ventanas de tiempo
 e={0:0,1:10,2:10,3:10,4:20,5:20,6:20,7:40,8:40,9:40,10:40} #minimo
 l={0:200,1:100,2:100,3:100,4:150,5:150,6:150,7:180,8:180,9:180,10:180}#maximo
@@ -25,18 +28,20 @@ s[0] =0
 X=np.random.rand(len(nodos))*100
 Y=np.random.rand(len(nodos))*100
 
-#Distancia
+#Distancias y tiempos
 distancia = {(i,j): np.hypot(X[i]-X[j],Y[i]-Y[j]) for i in nodos for j in nodos if i!=j}
+tiempo = {(i,j): np.hypot(X[i]-X[j],Y[i]-Y[j]) for i in nodos for j in nodos if i!=j}
+
 
 plt.figure(figsize=(12,5))
 plt.scatter(X,Y,color='blue')
 
 #DC
 plt.scatter(X[0],Y[0],color='red',marker='D')
-plt.annotate("DC",(X[0]-1,Y[0]-5.5))
+plt.annotate("DC|$t_{%d}$=(%d$,%d$)" %(0,e[0],l[0]),(X[0]-1,Y[0]-5.5))
 
 for i in clientes:
-    plt.annotate('$q_{%d}=%d$'%(i,q[i]),(X[i]-1,Y[i]-5.5))
+    plt.annotate('$q_{%d}=%d$|$t_{%d}$=(%d$,%d$)' %(i,q[i],i,e[i],l[i]),(X[i]-1,Y[i]-5.5))
 
 plt.xlabel("Distancia X")
 plt.ylabel("Distancia Y")
@@ -48,20 +53,29 @@ plt.show()
 prob = pulp.LpProblem("CVRP",pulp.LpMinimize)
 
 #Arco Set
-x = {i:pulp.LpVariable('a_{}'.format(i),cat ="Binary") for i in arcos}
+arco_var = [(i,j,k) for i in nodos for j in nodos for k in vehiculos if i!=j]
+arco_tiempos = [(i,k) for i in nodos for k in vehiculos]
+
 #Client Set
-u = {i:pulp.LpVariable('c_{}'.format(i),upBound=Q,cat ="Continuous" ) for i in clientes}
+x = {i:pulp.LpVariable('x_{}'.format(i),cat ="Binary") for i in arco_var} 
+t = {i:pulp.LpVariable('t_{}'.format(i),cat="Continuous") for i in arco_tiempos}
 
 
 
 #Objetive Function
-objective = pulp.lpSum(distancia[i,j]*x[i,j] for i,j in arcos)
-for i in clientes:
-    prob+= pulp.lpSum(x[i,j] for j in nodos if j!=i) == 1
+objective = pulp.lpSum(distancia[i,j]*x[i,j,k] for i,j,k in arco_var)
+
+#Restricciones
+#Entrada y salida de DC
 for j in clientes:
-    prob+= pulp.lpSum(x[i,j] for i in nodos if j!=i) == 1
+    prob+= pulp.lpSum(x[0,j,k]  for k in vehiculos if j!=i) <=1
+#salida
 for i in clientes:
-    prob+= u[i] >= q[i]
+    prob+= pulp.lpSum(x[i,0,k] for k in vehiculos if j!=i) <=1
+#vehiculo por nodo
+for i in clientes:
+    prob+= pulp.lpSum(x[i,j,k] for j in nodos for k in vehiculos if i!=j)==1 
+#Flujo cada nodo solo visita un nodo y cada nodo es visitado por un nodo
 
 prob.setObjective(objective)
 
